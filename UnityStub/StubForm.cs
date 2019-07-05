5513d6feac677b49f8fbfcb06590722f49dd095a
Dan B <me@narry.land>
2019-07-04 22:09:10 -0400

    Add dropped line of UI text

--- UnityStub/StubForm.cs
+++ UnityStub/StubForm.cs
@@ -69,6 +69,7 @@ namespace UnityStub
 
             UnityWatch.EnableInterface();
 
+            lbTarget.Text = UnityWatch.currentFileInfo.selectedTargetType.ToString() + " target loaded";
             lbTargetStatus.Text = UnityWatch.currentFileInfo.selectedTargetType.ToString() + " target loaded";
         }
 
